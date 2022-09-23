e57d63c718bc362ce74389de78ce3ced780ee37e
forestheims <forestheims@gmail.com>
2022-09-23 12:46:52 -0700

    favicon created and functioning on chrome and firefox

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home/Home';
+import Layout from './components/Layout/Layout'
 
 export default function App() {
   return (
     <Router>
+          <Layout>
         <Routes>
             <Route exact path="/" element={<Home />} />
         </Routes>
+          </Layout>
     </Router>
   );
 }
